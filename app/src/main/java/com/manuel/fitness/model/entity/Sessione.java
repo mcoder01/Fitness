6a3d0acfb8af0763350e334dddf84e3c9e987e71
Manuel <manu01141@gmail.com>
2022-07-31 23:31:36 +0200

    - Added support for android 12

--- app/src/main/java/com/manuel/fitness/model/entity/Sessione.java
+++ app/src/main/java/com/manuel/fitness/model/entity/Sessione.java
@@ -1,51 +1,35 @@
 package com.manuel.fitness.model.entity;
 
 import androidx.room.Entity;
-import androidx.room.ForeignKey;
 
-@Entity(tableName = "sessioni",
-        primaryKeys = {"idGiornata", "idEsercizio"},
-        foreignKeys = {
-            @ForeignKey(
-                    entity = Giornata.class,
-                    parentColumns = {"id"},
-                    childColumns = {"idGiornata"}),
-            @ForeignKey(
-                    entity = Esercizio.class,
-                    parentColumns = {"id"},
-                    childColumns = {"idEsercizio"}
-            )})
+@Entity(tableName = "sessioni", primaryKeys = {"idGiornata", "idEsercizio"})
 public class Sessione {
-    private long idGiornata;
-    private long idEsercizio;
-    private int ordinal;
+	private final long idGiornata;
+	private long idEsercizio;
+	private int ordinal;
 
-    public Sessione(long idGiornata, long idEsercizio) {
-        this.idGiornata = idGiornata;
-        this.idEsercizio = idEsercizio;
-    }
+	public Sessione(long idGiornata, long idEsercizio) {
+		this.idGiornata = idGiornata;
+		this.idEsercizio = idEsercizio;
+	}
 
-    public long getIdGiornata() {
-        return idGiornata;
-    }
+	public long getIdGiornata() {
+		return idGiornata;
+	}
 
-    public void setIdGiornata(long idGiornata) {
-        this.idGiornata = idGiornata;
-    }
+	public long getIdEsercizio() {
+		return idEsercizio;
+	}
 
-    public long getIdEsercizio() {
-        return idEsercizio;
-    }
+	public void setIdEsercizio(long idEsercizio) {
+		this.idEsercizio = idEsercizio;
+	}
 
-    public void setIdEsercizio(long idEsercizio) {
-        this.idEsercizio = idEsercizio;
-    }
+	public int getOrdinal() {
+		return ordinal;
+	}
 
-    public int getOrdinal() {
-        return ordinal;
-    }
-
-    public void setOrdinal(int ordinal) {
-        this.ordinal = ordinal;
-    }
+	public void setOrdinal(int ordinal) {
+		this.ordinal = ordinal;
+	}
 }
